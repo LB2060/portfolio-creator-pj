d1062864c8a9cadd76931fb7848d6b31f7812338
LB2060 <babayevaleman006@gmail.com>
2024-06-09 15:09:39 +0400

    portfolio-creator-exam

---
+++ assets/index.js
@@ -0,0 +1,129 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// section 2 -- companies
+
+const swiper = new Swiper(".companies-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+  });
+
+
+//   section 3 -- services
+
+const services = new Swiper(".cards-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+  });
+
+  //section 4 -- projects
+
+
+  const projects = new Swiper(".projectSwp", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 50,
+      },
+    },
+    navigation: {
+      nextEl: ".next-btn",
+      prevEl: ".prev-btn",
+    },
+  });
+
+ 
+//melumatlarin getirilmesi
+ window.addEventListener("DOMContentLoaded", comeinfo)
+const blog = document.getElementById("blog")
+async function comeinfo() {
+  const address = "https://jsonplaceholder.typicode.com/posts";
+
+  try {
+    const connect = await fetch(address);
+    const info = await connect.json();
+    console.log(info);
+
+    const blog = document.getElementById('blog');
+
+    for (let k = 0; k < info.length; k++) {
+      blog.innerHTML += `
+        <div class="blog">
+          <p><span id="date">April 16, 2021</span><span id="time">.6 mins</span></p>
+          <h2 class="py-2">${info[k].title}</h2>
+          <a href="#">Read the article <img class="px-4" src="./assets/pictures/section5-blogs/ox-white.svg" alt=""></a>
+        </div>`;
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+
+
+
+
+
